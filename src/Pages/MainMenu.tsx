290ed0bf82f297ff84a86b16673789672a703954
anas12312 <anashesham000@gmail.com>
2024-02-17 16:35:17 +0200

    anas:

--- src/Pages/MainMenu.tsx
+++ src/Pages/MainMenu.tsx
@@ -6,11 +6,12 @@ import { socketContext } from '../main'
 function MainMenu() {
   const [code, setCode] = useState('')
   const [error, setError] = useState('')
+  const [username, setUsername] = useState('')
   const socket = useContext(socketContext)
   const nav = useNavigate();
   useEffect(() => {
     socket.on('error', (data) => {
-        setError(data.message)
+      setError(data.message)
     })
     socket.on('joined', (data) => {
       nav('/room/' + data.roomId)
@@ -19,35 +20,55 @@ function MainMenu() {
       nav('/room/' + data.roomId)
     })
 
-  },[socket, nav])
+  }, [socket, nav])
   return (
     <div className='w-full h-full'>
       <div className='w-full h-full flex justify-start items-center flex-col'>
         <input
-         className='w-[21.5rem] h-10 p-2 pb-3 border border-black rounded-md'
-         value={code}
-         placeholder='Type the ID of the room'
-         onChange={(e) => {
-          setCode(e.target.value)
-         }}
+          className='w-[21.5rem] h-10 p-2 pb-3 border border-black rounded-md'
+          value={code}
+          placeholder='Type the ID of the room'
+          onChange={(e) => {
+            setCode(e.target.value)
+          }}
         />
-        <div className='flex'>  
+        <div className='flex'>
           <div className='w-40 h-20 flex justify-center items-center 
                         bg-gray-600 rounded-md text-white m-3 
                           hover:cursor-pointer hover:bg-slate-500'
-               onClick={() => {
-                socket.emit("join", {
-                  roomId: code
-                })
-               }}>Join Room</div>
+            onClick={() => {
+              socket.emit("join", {
+                roomId: code
+              })
+            }}>Join Room</div>
           <div className='w-40 h-20 flex justify-center items-center 
                         bg-gray-600 rounded-md text-white m-3 
                           hover:cursor-pointer hover:bg-slate-500'
-               onClick={() => {
-                socket.emit("create")
-               }}>Create Room</div>
+            onClick={() => {
+              socket.emit("create")
+            }}>Create Room</div>
         </div>
         <div>{error}</div>
+        <div>
+          <div>Change your Username</div>
+          <input
+            className='w-[21.5rem] h-10 p-2 pb-3 border border-black rounded-md'
+            value={username}
+            placeholder='Type the ID of the room'
+            onChange={(e) => {
+              setUsername(e.target.value)
+            }}
+          />
+          <div onClick={() => {
+            socket.emit('change_name', {
+              username
+            })
+          }} className='w-40 h-20 flex justify-center items-center 
+                        bg-gray-600 rounded-md text-white m-3 
+                        hover:cursor-pointer hover:bg-slate-500'>
+                          Save
+          </div>
+        </div>
       </div>
     </div>
   )
