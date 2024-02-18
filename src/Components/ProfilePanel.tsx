d16e82e91dbb450fb26381cbf11b91d9dce329e3
anas12312 <anashesham000@gmail.com>
2024-02-18 17:26:49 +0200

    anas

--- src/Components/ProfilePanel.tsx
+++ src/Components/ProfilePanel.tsx
@@ -19,7 +19,7 @@ export default function ProfilePanel({ leaveRoom, username, image }: ProfilePane
 
     return (
         <>
-            <SettingsModal 
+            <SettingsModal
                 isOpen={settingsModal}
                 setIsOpen={setSettingModal}
                 oldUsername={username}
@@ -35,52 +35,51 @@ export default function ProfilePanel({ leaveRoom, username, image }: ProfilePane
                     </div>
 
 
-                <div onClick={() => {
-                    leaveRoom()
-                }} className='w-8 h-[65%] rounded-md flex justify-center items-center hover:bg-slate-800 hover:cursor-pointer'>
-                    <img src='./icons/hangup.svg' className='w-6 h-6' />
-                </div>
-            </div >
-
-                    <div className='w-8 h-[65%] rounded-md flex justify-center items-center hover:bg-slate-800 hover:cursor-pointer'>
+                    <div onClick={() => {
+                        leaveRoom()
+                    }} className='w-8 h-[65%] rounded-md flex justify-center items-center hover:bg-slate-800 hover:cursor-pointer'>
                         <img src='./icons/hangup.svg' className='w-6 h-6' />
                     </div>
                 </div >
 
-                {/* Profile & Mute */}
-                < div className='h-[55%] pl-2 flex justify-center items-center border-t border-slate-800 bg-slate-950' >
+                <div className='w-8 h-[65%] rounded-md flex justify-center items-center hover:bg-slate-800 hover:cursor-pointer'>
+                    <img src='./icons/hangup.svg' className='w-6 h-6' />
+                </div>
+            </div >
 
-                    <div className='w-[50%] flex justify-start space-x-3 items-center pl-2 h-10 rounded-md hover:bg-slate-800 hover:cursor-pointer'>
+            {/* Profile & Mute */}
+            < div className='h-[55%] pl-2 flex justify-center items-center border-t border-slate-800 bg-slate-950' >
 
-                        <div className='rounded-full w-8 h-8'>
-                            <img src={image} className='w-8 h-8 rounded-full object-cover' />
-                        </div>
+                <div className='w-[50%] flex justify-start space-x-3 items-center pl-2 h-10 rounded-md hover:bg-slate-800 hover:cursor-pointer'>
 
-                        <div className='font-main text-white select-none'>
-                            {username}
-                        </div>
-
+                    <div className='rounded-full w-8 h-8'>
+                        <img src={image} className='w-8 h-8 rounded-full object-cover' />
                     </div>
 
-                    <div className='w-[50%] h-full px-2 flex justify-center items-center'>
-                        <div className='w-full h-[65%] rounded-md flex justify-center items-center hover:bg-slate-800 hover:cursor-pointer select-none'>
-                            <img src='./icons/mic.svg' className='w-6 h-6' />
-                        </div>
-                        <div className='w-full h-[65%] rounded-md flex justify-center items-center hover:bg-slate-800 hover:cursor-pointer select-none'>
-                            <img src='./icons/headset.svg' className='w-6 h-6' />
-                        </div>
-                        <div 
+                    <div className='font-main text-white select-none'>
+                        {username}
+                    </div>
+
+                </div>
+
+                <div className='w-[50%] h-full px-2 flex justify-center items-center'>
+                    <div className='w-full h-[65%] rounded-md flex justify-center items-center hover:bg-slate-800 hover:cursor-pointer select-none'>
+                        <img src='./icons/mic.svg' className='w-6 h-6' />
+                    </div>
+                    <div className='w-full h-[65%] rounded-md flex justify-center items-center hover:bg-slate-800 hover:cursor-pointer select-none'>
+                        <img src='./icons/headset.svg' className='w-6 h-6' />
+                    </div>
+                    <div
                         onClick={openSettingModal}
                         className='w-full h-[65%] rounded-md flex justify-center items-center hover:bg-slate-800 hover:cursor-pointer select-none'>
-                            <img src={'./icons/settings.svg'} className='w-6 h-6' />
-                        </div>
-
+                        <img src={'./icons/settings.svg'} className='w-6 h-6' />
                     </div>
 
+                </div>
 
-                </div >
 
             </div >
+
         </>
     )
 }
