64b8a871d9076081d8a4c654b3f9b203aedbe57b
anas12312 <anashesham000@gmail.com>
2024-03-03 15:16:12 +0200

    anas

--- src/Components/Player/Track.tsx
+++ src/Components/Player/Track.tsx
@@ -7,32 +7,61 @@ export interface track {
     author: string
 }
 interface props extends track {
-    handleDragStart: Function, 
-    handleDragOver: DragEventHandler<HTMLDivElement>, 
-    handleDrop: Function, 
-    index: number
+    handleDragStart: Function,
+    handleDragOver: DragEventHandler<HTMLDivElement>,
+    handleDrop: Function,
+    index: number,
+    playing?: boolean,
+    deleteTrack: Function,
+    playTrack: Function
 }
 export function formatTime(seconds: number) {
     const minutes = Math.floor(seconds / 60);
     const remainingSeconds = seconds % 60;
     return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
-  }
-export default function Track({ name, url, thumbnail, handleDragStart, handleDragOver, handleDrop, index, author, duration}: props) {
+}
+export default function Track({ name, playing, url, deleteTrack, playTrack, thumbnail, handleDragStart, handleDragOver, handleDrop, index, author, duration }: props) {
     return (
-        <div draggable
-            key={index}
-            onDragStart={(event) => handleDragStart(event, index)}
-            onDragOver={handleDragOver}
-            onDrop={(event) => handleDrop(event, index)}
-            className='w-full h-20 mt-1 bg-slate-700 p-2 flex items-center'>
-            <div>
-                <img className='w-28' src={thumbnail} alt="" />
-            </div>
-            <div className='ml-2 flex flex-col h-full w-[60%]'>
-                <div className='text-base font-bold truncate'>{name}</div>
-                <div className='text-sm'>{author}</div>
-                <div className='text-sm'>{formatTime(duration)}</div>
-            </div>
+        <div className='w-full'>
+            {playing ? (
+                <div draggable
+                    key={index}
+                    onDragStart={(event) => handleDragStart(event, index)}
+                    onDragOver={handleDragOver}
+                    onDrop={(event) => handleDrop(event, index)}
+                    className='w-full h-20 mt-1 bg-green-800 p-2 flex items-center relative'>
+                    <div>
+                        <img className='w-28' src={thumbnail} alt="" />
+                    </div>
+                    <div className='ml-2 flex flex-col h-full w-[60%]'>
+                        <div className='text-base font-bold truncate'>{name}</div>
+                        <div className='text-sm'>{author}</div>
+                        <div className='text-sm'>{formatTime(duration)}</div>
+                    </div>
+                    <div className='absolute top-[40%] right-3 p-2 hover:bg-red-800 cursor-pointer rounded-lg'>
+                        <img className='w-6' src='../../../public/icons/remove.png' />
+                    </div>
+                </div>
+            ) : (
+                <div draggable
+                    key={index}
+                    onDragStart={(event) => handleDragStart(event, index)}
+                    onDragOver={handleDragOver}
+                    onDrop={(event) => handleDrop(event, index)}
+                    className='w-full h-20 mt-1 bg-slate-700 p-2 flex items-center relative'>
+                    <div>
+                        <img className='w-28' src={thumbnail} alt="" />
+                    </div>
+                    <div className='ml-2 flex flex-col h-full w-[60%]'>
+                        <div className='text-base font-bold truncate'>{name}</div>
+                        <div className='text-sm'>{author}</div>
+                        <div className='text-sm'>{formatTime(duration)}</div>
+                    </div>
+                    <div className='absolute top-[40%] right-3 hover:bg-red-800 p-2 cursor-pointer rounded-lg'>
+                        <img className='w-6' src='../../../public/icons/remove.png' />
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
