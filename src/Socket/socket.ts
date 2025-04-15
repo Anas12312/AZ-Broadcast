b2283e53de5e2b4696723ae9161ce28151af1929
zyad007 <zyad.sallem.007@gmail.com>
2025-04-15 12:52:03 +0200

    fix

--- src/Socket/socket.ts
+++ src/Socket/socket.ts
@@ -2,7 +2,8 @@ import { io } from 'socket.io-client';
 import Cookies from 'cookies-js';
 
 // export const BASE_URL = 'http://localhost:4000'
-export const BASE_URL = "https://az-broadcast-backend.onrender.com";
+// export const BASE_URL = "https://az-broadcast-backend.onrender.com";
+export const BASE_URL = 'https://chubby-wombats-train.loca.lt'
 
 export const socket = io(BASE_URL);
 
